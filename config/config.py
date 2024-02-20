dd9058866de410f568c6c5dd5b1732a515b32a8f
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-02-20 21:37:08 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -54,9 +54,9 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/redeyeservicegroup"
+    "SUPPORT_CHANNEL", "https://t.me/Marin_probot_support"
 )  # Example:- https://t.me/HydraXSupport
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/redeyeservicegroup")  # Example:- https://t.me/HydraXSupport
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Marin_probot_support")  # Example:- https://t.me/HydraXSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
